92db3ad81e0c841ff2bd1046ebda4c1b9f289934
Navneet-Singh57 <navneet.vips57@gmail.com>
2024-08-16 22:05:36 +0530

    Commit

--- IMDB/IMDB/settings.py
+++ IMDB/IMDB/settings.py
@@ -134,16 +134,20 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         #'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-        'user': '300/day'
-    }
+        'user': '100/day',
+        'review-create': '2/day',
+        'review-list': '100/day',
+        'review-detail': '100/day',
+    },
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
-# SIMPLE_JWT = {
-#     'ROTATE_REFRESH_TOKEN': True
-# }
+
